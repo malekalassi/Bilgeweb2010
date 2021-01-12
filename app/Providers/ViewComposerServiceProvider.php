212d078fd535dfd1778623d4b19084231c81583e
malekalassi <35423810+malekalassi@users.noreply.github.com>
2021-01-12 10:56:56 +0300

    Share Cart count across all views

---
+++ app/Providers/ViewComposerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Category;
+
+use Darryldecode\Cart\Cart;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewComposerServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('site.partials.nav', function ($view) {
+            $view->with('categories', Category::orderByRaw('-name ASC')->get()->nest());
+        });
+        View::composer('site.partials.header' , function ($view){
+            $view->with('cartCount', \Cart::count());
+        });
+    }
+}
