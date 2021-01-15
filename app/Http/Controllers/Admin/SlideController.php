6c984e2b5d5ad70453f393f000c80dc13f3b2802
malekalassi <35423810+malekalassi@users.noreply.github.com>
2021-01-15 17:22:27 +0300

    work over store method in slide resource

--- app/Http/Controllers/Admin/SlideController.php
+++ app/Http/Controllers/Admin/SlideController.php
@@ -34,7 +34,21 @@ class SlideController extends BaseController
 
     public function store(Request $request)
     {
-        dd($request);
+//        dd($request);
+        $this->validate($request , [
+            'title'      =>  'required|max:191',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+        $params = $request->except('_token');
+
+        $slide = $this->slideRepository->createSlide($params);
+
+
+
+        if (!$slide){
+            return $this->responseRedirectBack('Error occurred while creating brand.' ,'error' , true , true);
+        }
+        return  $this->responseRedirect('admin.slides.index' ,'success', 'slide add successfully' ,false , false);
     }
 
 }
