f2866f8d3bf3691630a02b61394c8e3cff44bc5a
malekalassi <35423810+malekalassi@users.noreply.github.com>
2021-01-18 14:56:27 +0300

    Work over cart resource

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+//        $this->middleware('auth');
     }
 
     /**
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+//          $products =Product::with('categories' ,'attributes.attributesValues.attribute' )->get();
+        $products =Product::latest()->take(4)->get();
+
+
+
+        return view('site.pages.homepage' ,compact('products'));
     }
 }
